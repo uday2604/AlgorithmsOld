d603dfbc3b568d69537653a9ac71fa8bb3c1f1d6
Uday Thota <udaydeep.thota@gmail.com>
2019-03-15 02:07:20 -0700

    update the path sum code

--- TreesPractice/src/com/amazon/PathSumInBinaryTree.java
+++ TreesPractice/src/com/amazon/PathSumInBinaryTree.java
@@ -5,72 +5,51 @@ import java.util.Queue;
 
 /**
  * Created by uthota on 8/26/16.
+ * Return if the tree contains a path from the root to leaf where the sum of its nodes is equal to passed value - iterative approach
+ * TODO: think of recursive / memory optimized approach
  */
 public class PathSumInBinaryTree {
 
     static class BinaryTreeNode {
-
         BinaryTreeNode llink;
         BinaryTreeNode rlink;
         int data;
 
         BinaryTreeNode(int data) {
-
-            this.data=data;
-            this.llink=null;
-            this.rlink=null;
+            this.data = data;
+            this.llink = null;
+            this.rlink = null;
         }
-
     }
 
-    public boolean sumPathExistsFromRootToLeaf(BinaryTreeNode root, int sumValue) {
-
-
-        if(root==null)
+    private boolean sumPathExistsFromRootToLeaf(BinaryTreeNode root, int sumValue) {
+        if (root == null)
             return false;
-
         else {
-
             Queue<BinaryTreeNode> nodeStoreQueue = new LinkedList<>();
-            Queue<Integer>nodeValueStore= new LinkedList<>();
-
+            Queue<Integer> nodeValueStore = new LinkedList<>();
             nodeStoreQueue.offer(root);
             nodeValueStore.offer(root.data);
-
             while (!nodeStoreQueue.isEmpty()) {
-
                 BinaryTreeNode temp = nodeStoreQueue.poll();
-                Integer sumTillThatNode=nodeValueStore.poll();
-
-
+                Integer sumTillThatNode = nodeValueStore.poll();
                 // Return true if the node is leaf node and the sum value of the node (leaf) is target value
-                if(temp.llink==null && temp.rlink==null && sumTillThatNode==sumValue)
+                if (temp.llink == null && temp.rlink == null && sumTillThatNode == sumValue)
                     return true;
-
-                if(temp.llink!=null) {
+                if (temp.llink != null) {
                     nodeStoreQueue.offer(temp.llink);
-                    nodeValueStore.offer(temp.llink.data+sumTillThatNode);
-
+                    nodeValueStore.offer(temp.llink.data + sumTillThatNode);
                 }
-
-                if(temp.rlink!=null) {
-
+                if (temp.rlink != null) {
                     nodeStoreQueue.offer(temp.rlink);
-                    nodeValueStore.offer(temp.rlink.data+sumTillThatNode);
-
+                    nodeValueStore.offer(temp.rlink.data + sumTillThatNode);
                 }
-
             }
-
             return false;
-
         }
-
-
     }
 
     public static void main(String[] args) {
-
         BinaryTreeNode root = new BinaryTreeNode(5);
         BinaryTreeNode node4 = new BinaryTreeNode(4);
         BinaryTreeNode node5 = new BinaryTreeNode(8);
@@ -80,25 +59,18 @@ public class PathSumInBinaryTree {
         BinaryTreeNode node9 = new BinaryTreeNode(7);
         BinaryTreeNode node10 = new BinaryTreeNode(2);
         BinaryTreeNode node11 = new BinaryTreeNode(1);
-
         root.llink = node4;
         root.rlink = node5;
-
         node4.llink = node6;
-        node4.rlink=null;
-
-        node5.llink=node7;
-        node5.rlink=node8;
-
-        node6.llink=node9;
-        node6.rlink=node10;
-
-        node8.llink=null;
-        node8.rlink=node11;
-
+        node4.rlink = null;
+        node5.llink = node7;
+        node5.rlink = node8;
+        node6.llink = node9;
+        node6.rlink = node10;
+        node8.llink = null;
+        node8.rlink = node11;
         PathSumInBinaryTree pathSumInBinaryTree = new PathSumInBinaryTree();
-        boolean doesPathExist=pathSumInBinaryTree.sumPathExistsFromRootToLeaf(root, 22);
-        System.out.println("The given sum path from root to leaf exists in the tree: "+doesPathExist);
-
+        boolean doesPathExist = pathSumInBinaryTree.sumPathExistsFromRootToLeaf(root, 22);
+        System.out.println("The given sum path from root to leaf exists in the tree: " + doesPathExist);
     }
 }
